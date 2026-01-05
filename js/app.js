1fd6ad5c293e9f43a0675f40cc3f2018e54b2c52
TomatoCakePasta <gaomuk50@gmail.com>
2026-01-06 00:26:15 +0900

    実装:注文モーダル

--- js/app.js
+++ js/app.js
@@ -16,11 +16,18 @@ import { MENU_DATA } from "./data.js";
 
 let cart = {};
 let currentCategory = MENU_DATA.categories[0];
+let total = 0;
+
+let cart_total;
+let cart_history = [];
 
 // ===== 初期表示 =====
 const categoriesEl = document.getElementById("categories");
 const menuEl = document.getElementById("menu");
 const orderCounterEl = document.getElementById("order-counter");
+
+const modal = document.getElementById("order-modal");
+const orderList = document.getElementById("order-list");
 
 // カテゴリ描画
 MENU_DATA.categories.forEach(category => {
@@ -30,6 +37,8 @@ MENU_DATA.categories.forEach(category => {
     div.onclick = () => selectCategory(category);
     categoriesEl.appendChild(div);
 });
+
+console.log("app.js loaded");
 
 selectCategory(currentCategory);
 
@@ -51,7 +60,7 @@ function selectCategory(category) {
             <img src="${item.image}" alt="${item.name}の画像を作ってください">
             <h3>${item.name}</h3>
             <p>¥${item.price}</p>
-            <button>追加</button>
+            <button class="add-menu">追加</button>
         `;
         div.querySelector("button").onclick = () => addToCart(item);
         menuEl.appendChild(div);
@@ -70,6 +79,12 @@ function addToCart(item) {
     cart[item.id].qty++;
     renderCart();
     countCart();
+    addHistory(item);
+}
+
+function addHistory(item) {
+    cart_history.push(item);
+    console.log(cart_history);
 }
 
 function countCart() {
@@ -87,11 +102,12 @@ function renderCart() {
     const totalEl = document.getElementById("total");
 
     cartItemsEl.innerHTML = "";
-    let total = 0;
+
+    cart_total = 0;
 
     Object.values(cart).forEach(item => {
         const itemTotal = item.price * item.qty;
-        total += itemTotal;
+        cart_total += itemTotal;
 
         const div = document.createElement("div");
         div.className = "cart-item";
@@ -102,5 +118,67 @@ function renderCart() {
         cartItemsEl.appendChild(div);
     });
 
-    totalEl.textContent = total;
+    totalEl.textContent = total + cart_total;
+}
+
+// モーダル関連
+window.openOrderModal = function () {
+    modal.classList.remove("hidden");
+    renderOrderList();
+}
+
+// 金額が1多いかも
+window.addLastCart = function (id) {
+    cart[id].qty++;
+    countCart();
+    renderCart();
+    renderOrderList();
+}
+
+window.subLastCart = function (id) {
+    if (cart[id].qty < 1) {
+        return;
+    }
+
+    cart[id].qty--;
+    countCart();
+    renderCart();
+    renderOrderList();
+}
+
+function renderOrderList() {
+    orderList.innerHTML = "";
+
+    Object.entries(cart).forEach(([id, item]) => {
+        const row = document.createElement("div");
+        row.className = "order-item";
+
+        row.innerHTML = `
+            <span>${item.name}</span>
+            <div class="qty-controls">
+                <button onclick="subLastCart('${id}')">−</button>
+                <span>${item.qty}</span>
+                <button onclick="addLastCart('${id}')">＋</button>
+            </div>
+        `;
+
+        orderList.appendChild(row);
+    });
+}
+
+// 注文確定
+window.confirmOrder = function () {
+    modal.classList.add("hidden");
+    calcTotal();
+    resetCart();
+    countCart();
+    renderCart();
+}
+
+function calcTotal() {
+    total += cart_total;
+}
+
+function resetCart() {
+    cart = {};
 }
